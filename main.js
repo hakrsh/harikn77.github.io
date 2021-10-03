8812386f7b1728943659884acba7a556ed130637
hari <harikn77@gmail.com>
2021-10-03 22:46:45 +0530

    v1

---
+++ main.js
@@ -0,0 +1,31 @@
+let mainNav = document.getElementById('main-nav');
+let navbarToggle = document.getElementById('navbar-toggle');
+
+
+
+navbarToggle.addEventListener('click', function() {
+
+    if (this.classList.contains('active')) {
+        mainNav.style.display = "none";
+        this.classList.remove('active');
+    } else {
+        mainNav.style.display = "flex";
+        this.classList.add('active');
+
+    }
+});
+
+function date() {
+    document.querySelector('footer').innerHTML = new Date();
+}
+
+setInterval(date, 1000);
+
+load = (id) => {
+    const divIds = ['one', 'two', 'three', 'four', 'five']
+    divIds.forEach((id) => {
+        document.getElementById(id).style.display = 'none';
+    });
+    if (id === 'logo') document.getElementById('one').style.display = 'Block';
+    else document.getElementById(divIds[id]).style.display = 'Block';
+}
